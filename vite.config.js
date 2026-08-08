06f62d79eafcd03855c262b2b44c2fd2e2ecc776
leenguyenzz <leenguyen.nll@gmail.com>
2026-08-08 10:24:26 +0700

    Configure base path / and CNAME for custom domain loichuamoingay.org

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 import { BRAND } from './src/config/brand.js';
 
 export default defineConfig({
-  base: '/loi-chua-hang-ngay/',
+  base: '/',
   plugins: [
     react(), 
     tailwindcss(),
